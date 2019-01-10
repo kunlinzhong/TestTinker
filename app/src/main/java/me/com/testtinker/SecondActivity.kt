55d568fab9790a6c5fb5c5a7360c3baa8084533f
zhongkunlin@comodin.cn <zhongkunlin@comodin.cn>
2019-01-10 18:27:47 +0800

    添加gif图片

--- app/src/main/java/me/com/testtinker/SecondActivity.kt
+++ app/src/main/java/me/com/testtinker/SecondActivity.kt
@@ -1,6 +1,8 @@
 package me.com.testtinker
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -16,25 +18,44 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import me.com.t.StrUtil
 import java.lang.reflect.InvocationTargetException
-import me.com.baselibrary.AutoTouch
+import android.widget.Toast
+import com.tinkerpatch.sdk.server.utils.c.r
+import android.graphics.Bitmap
+
+
+
 
 class SecondActivity : AppCompatActivity(),View.OnClickListener {
     override fun onClick(v: View?) {
     when (v?.id){
-        R.id.imageView ->{
+        R.id.pngLinearlayout ->{
 //            Glide.with(this).load("https://upload-images.jianshu.io/upload_images/5885602-edb0d53965efd964.gif").into(imageView);
-//            loadOneTimeGif(this,R.drawable.ic_gif_2,imageView,gifListener);
-            Glide.with(this).load(R.drawable.ic_gif_2).into(imageView);
-            startSplashThread()
+            svgImageView.setImageResource(R.drawable.ic_home_unselect)
+            loadOneTimeGif(this,R.drawable.ic_home_active,imageView,pngGifListener);
+
+//            Glide.with(this).load(R.drawable.ic_gif_2).into(imageView);
+//            startSplashThread()
+        }
+        R.id.svgLinearlayout->{
+            imageView.setImageResource(R.mipmap.ic_home_unselect)
+            loadSvgOneTimeGif(this,R.drawable.ic_home_active,svgImageView,svgGifListener);
         }
     }
     }
 
-    val gifListener = object : GifListener {
+    val pngGifListener = object : GifListener {
+        @SuppressLint("ResourceType")
         override fun gifPlayComplete() {
-            Glide.with(this@SecondActivity).load(R.drawable.ic_gif).into(imageView);
+            Toast.makeText(this@SecondActivity,"pngImageView-->",Toast.LENGTH_SHORT).show()
+            imageView.setImageResource(R.mipmap.ic_home_select)
+        }
+    }
+
+    val svgGifListener = object : GifListener {
+        @SuppressLint("ResourceType")
+        override fun gifPlayComplete() {
+            svgImageView.setImageResource(R.drawable.ic_home_select)
         }
     }
     fun startSplashThread(){
@@ -54,6 +75,7 @@ class SecondActivity : AppCompatActivity(),View.OnClickListener {
         }
         splashThread.start()
     }
+
     // 创建一个Handler
     private val handler: Handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message?) {
@@ -61,6 +83,7 @@ class SecondActivity : AppCompatActivity(),View.OnClickListener {
             when (msg?.what) {
                 SPLASH_WHAT -> {
                     Glide.with(this@SecondActivity).load(R.drawable.ic_gif).into(imageView);
+//                    imageView.setBackgroundResource(R.mipmap.ic_home_select)
                 } else -> { // 这里的else相当于Java中switch的default;
                     val mBundle = msg?.data
                 }
@@ -75,8 +98,8 @@ class SecondActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     fun initView(){
-        imageView.setOnClickListener(this)
-        Glide.with(this@SecondActivity).load(R.drawable.ic_gif).into(imageView);
+        pngLinearlayout.setOnClickListener(this)
+        svgLinearlayout.setOnClickListener(this)
 //        val autoTouch = AutoTouch()
 //        autoTouch.autoClickPos(0.0, 0.0, 500.0, 500.0)
 //        StrUtil.testInt(1,"--->")
@@ -86,63 +109,120 @@ class SecondActivity : AppCompatActivity(),View.OnClickListener {
         super.onDestroy()
         Glide.get(this).clearMemory();
     }
-
-    fun loadOneTimeGif(context: Context, model: Any, imageView: ImageView, gifListener: GifListener?) {
-        Glide.with(context).asGif().load(model).listener(object : RequestListener<GifDrawable> {
-            override fun onLoadFailed(
-                e: GlideException?,
-                model: Any,
-                target: Target<GifDrawable>,
-                isFirstResource: Boolean
-            ): Boolean {
-                return false
-            }
-
-            override fun onResourceReady(
-                resource: GifDrawable,
-                model: Any,
-                target: Target<GifDrawable>,
-                dataSource: DataSource,
-                isFirstResource: Boolean
-            ): Boolean {
-                try {
-                    val gifStateField = GifDrawable::class.java.getDeclaredField("state")
-                    gifStateField.isAccessible = true
-                    val gifStateClass = Class.forName("com.bumptech.glide.load.resource.gif.GifDrawable\$GifState")
-                    val gifFrameLoaderField = gifStateClass.getDeclaredField("frameLoader")
-                    gifFrameLoaderField.isAccessible = true
-                    val gifFrameLoaderClass = Class.forName("com.bumptech.glide.load.resource.gif.GifFrameLoader")
-                    val gifDecoderField = gifFrameLoaderClass.getDeclaredField("gifDecoder")
-                    gifDecoderField.isAccessible = true
-                    val gifDecoderClass = Class.forName("com.bumptech.glide.gifdecoder.GifDecoder")
-                    val gifDecoder = gifDecoderField.get(gifFrameLoaderField.get(gifStateField.get(resource)))
-                    val getDelayMethod = gifDecoderClass.getDeclaredMethod("getDelay", Int::class.javaPrimitiveType)
-                    getDelayMethod.isAccessible = true
-                    //设置只播放一次
-                    resource.setLoopCount(1)
-                    //获得总帧数
-                    val count = resource.frameCount
-                    var delay = 0
-                    for (i in 0 until count) {
-                        //计算每一帧所需要的时间进行累加
-                        delay += getDelayMethod.invoke(gifDecoder, i) as Int
-                    }
-                    imageView.postDelayed({
-                        gifListener?.gifPlayComplete()
-                    }, delay.toLong())
-                } catch (e: NoSuchFieldException) {
-                    e.printStackTrace()
-                } catch (e: ClassNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                } catch (e: InvocationTargetException) {
-                    e.printStackTrace()
+    companion object {
+        fun loadOneTimeGif(context: Context, model: Any, mImageView: ImageView, gifListener: GifListener?) {
+            Glide.with(context).asGif().load(model).listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any,
+                    target: Target<GifDrawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
                 }
 
-                return false
-            }
-        }).into(imageView)
+                override fun onResourceReady(
+                    resource: GifDrawable,
+                    model: Any,
+                    target: Target<GifDrawable>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    try {
+                        val gifStateField = GifDrawable::class.java.getDeclaredField("state")
+                        gifStateField.isAccessible = true
+                        val gifStateClass = Class.forName("com.bumptech.glide.load.resource.gif.GifDrawable\$GifState")
+                        val gifFrameLoaderField = gifStateClass.getDeclaredField("frameLoader")
+                        gifFrameLoaderField.isAccessible = true
+                        val gifFrameLoaderClass = Class.forName("com.bumptech.glide.load.resource.gif.GifFrameLoader")
+                        val gifDecoderField = gifFrameLoaderClass.getDeclaredField("gifDecoder")
+                        gifDecoderField.isAccessible = true
+                        val gifDecoderClass = Class.forName("com.bumptech.glide.gifdecoder.GifDecoder")
+                        val gifDecoder = gifDecoderField.get(gifFrameLoaderField.get(gifStateField.get(resource)))
+                        val getDelayMethod = gifDecoderClass.getDeclaredMethod("getDelay", Int::class.javaPrimitiveType)
+                        getDelayMethod.isAccessible = true
+                        //设置只播放一次
+                        resource.setLoopCount(1)
+                        //获得总帧数
+                        val count = resource.frameCount
+                        var delay = 0
+                        for (i in 0 until count) {
+                            //计算每一帧所需要的时间进行累加
+                            delay += getDelayMethod.invoke(gifDecoder, i) as Int
+                        }
+                        mImageView.postDelayed({
+                            gifListener?.gifPlayComplete()
+                        }, delay.toLong())
+                    } catch (e: NoSuchFieldException) {
+                        e.printStackTrace()
+                    } catch (e: ClassNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IllegalAccessException) {
+                        e.printStackTrace()
+                    } catch (e: InvocationTargetException) {
+                        e.printStackTrace()
+                    }
+                    return false
+                }
+            }).into(mImageView)
+        }
+
+        fun loadSvgOneTimeGif(context: Context, model: Any, mImageView: ImageView, gifListener: GifListener?) {
+            Glide.with(context).asGif().load(model).listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any,
+                    target: Target<GifDrawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: GifDrawable,
+                    model: Any,
+                    target: Target<GifDrawable>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    try {
+                        val gifStateField = GifDrawable::class.java.getDeclaredField("state")
+                        gifStateField.isAccessible = true
+                        val gifStateClass = Class.forName("com.bumptech.glide.load.resource.gif.GifDrawable\$GifState")
+                        val gifFrameLoaderField = gifStateClass.getDeclaredField("frameLoader")
+                        gifFrameLoaderField.isAccessible = true
+                        val gifFrameLoaderClass = Class.forName("com.bumptech.glide.load.resource.gif.GifFrameLoader")
+                        val gifDecoderField = gifFrameLoaderClass.getDeclaredField("gifDecoder")
+                        gifDecoderField.isAccessible = true
+                        val gifDecoderClass = Class.forName("com.bumptech.glide.gifdecoder.GifDecoder")
+                        val gifDecoder = gifDecoderField.get(gifFrameLoaderField.get(gifStateField.get(resource)))
+                        val getDelayMethod = gifDecoderClass.getDeclaredMethod("getDelay", Int::class.javaPrimitiveType)
+                        getDelayMethod.isAccessible = true
+                        //设置只播放一次
+                        resource.setLoopCount(1)
+                        //获得总帧数
+                        val count = resource.frameCount
+                        var delay = 0
+                        for (i in 0 until count) {
+                            //计算每一帧所需要的时间进行累加
+                            delay += getDelayMethod.invoke(gifDecoder, i) as Int
+                        }
+                        mImageView.postDelayed({
+                            gifListener?.gifPlayComplete()
+                        }, delay.toLong())
+                    } catch (e: NoSuchFieldException) {
+                        e.printStackTrace()
+                    } catch (e: ClassNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IllegalAccessException) {
+                        e.printStackTrace()
+                    } catch (e: InvocationTargetException) {
+                        e.printStackTrace()
+                    }
+                    return false
+                }
+            }).into(mImageView)
+        }
     }
 
     /**
